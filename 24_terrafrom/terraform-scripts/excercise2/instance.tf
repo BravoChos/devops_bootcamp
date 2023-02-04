c99383593f7c5cc50c751609378bd317c9df78a7
BravoChos <bravochos@gmail.com>
2023-02-05 04:33:21 +0900

    terraform/excercise2 - create ssh-keygen

--- 24_terrafrom/terraform-scripts/excercise2/instance.tf
+++ 24_terrafrom/terraform-scripts/excercise2/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "pineapple-inst" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
-  key_name               = "Pineapple"
+  key_name               = "new-pineapple"
   vpc_security_group_ids = ["sg-087cc3b2b7c5d720b"]
   tags = {
     Name    = "Pineapple-Instance"
